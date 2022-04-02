4a27841f340b5d152778996cee3a6ce8084734ef
Amir Hachaichi <amir.hac@outlook.com>
2022-04-02 17:35:28 +0200

    homepage update

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
 
           <div class="pl-4">
             <a class="text-gray-900 text-base no-underline hover:no-underline font-extrabold text-xl" href="#">
-              home
+              Amir's homepage
             </a>
           </div>
           
@@ -20,17 +20,53 @@
 
           <div class="font-sans">
             
-                <h1 class="font-bold font-sans break-normal text-gray-900 pt-6 pb-2 text-3xl md:text-4xl"></h1>
-                <p class="text-sm md:text-base font-normal text-gray-600">Published </p>
+                <h1 class="font-bold font-sans break-normal text-gray-900 pt-6 pb-2 text-3xl md:text-4xl">Tech & Life Sciences</h1>
+                
           </div>
-
-          <nuxt-content :document="article" class="py-6" />
 
       </div>
 
-      <hr class="border-b-2 border-gray-400 mb-8 mx-4">
-
     </div>
+
+    <footer class="bg-white border-t border-gray-400 shadow">
+      <div class="container max-w-4xl mx-auto flex py-8">
+
+        <div class="w-full mx-auto flex flex-wrap">
+
+          <div class="flex w-full md:w-1/2 ">
+            <div class="px-8">
+
+              <h3 class="font-bold text-gray-900">Amir Hachaichi</h3>
+              <p class="py-4 text-gray-600 text-sm">
+                Copyright 2022
+              </p>
+
+            </div>
+          </div>
+
+          <div class="flex w-full md:w-1/2">
+            <div class="px-8">
+
+              <h3 class="font-bold text-gray-900">Social</h3>
+              <ul class="list-reset items-center text-sm pt-3">
+                <li>
+                  <a class="inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-1" href="#">LinkedIn</a>
+                </li>
+                <li>
+                  <a class="inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-1" href="#">Medium</a>
+                </li>
+                <li>
+                  <a class="inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-1" href="#">Twitter</a>
+                </li>
+              </ul>
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </footer>
 
   </home>
 </template>
